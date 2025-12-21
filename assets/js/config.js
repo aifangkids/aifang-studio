444af5cb1b868de05ba9ed00ca21bea4259e267a
aifangkids <aifangkids@gmail.com>
2025-12-21 23:51:35 +0800

    24

---
+++ assets/js/config.js
@@ -0,0 +1,4 @@
+// 全站設定，只放不會動的東西
+const GAS_URL = "https://script.google.com/macros/s/AKfycbxrmloTY4wCo1Sn5tgMQDRwhU8uXWBTA0c6v17ec7M6W5LkufjES1fjJBolMb_552z5/exec";
+
+console.log("config.js loaded");
---
+++ assets/js/render.js
@@ -0,0 +1,2 @@
+// 專門負責畫畫面（暫時不畫）
+console.log("render.js loaded");
